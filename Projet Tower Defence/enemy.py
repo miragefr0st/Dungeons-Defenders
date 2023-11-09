0e5572f6af5cfbf1999b654fdf14691ffa13fe88
Mamitiana RASOLOJAONA <iaesensei@yahoo.com>
2023-11-09 16:48:03 +0100

    Delete Projet Tower Defence directory

--- Projet Tower Defence/enemy.py
+++
@@ -1,8 +0,0 @@
-import pygame as pg
-
-class Enemy(pg.sprite.Sprite):
-    def _init_(self, pos, image):
-        pg.sprite.Sprite._init_(self)
-        self.image = image
-        self.rect = self.image.get_rect()
-        self.rect.center = pos
